63f6c062936edfc13bd0f809500ea85f7c5f985d
Patryk Bombik <patryk.bombik@gmail.com>
2024-05-09 13:34:32 +0200

    added models and routes

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,14 +1,21 @@
 import express from 'express';
+import Subscriber from '../models/subscriber.js';
 
 const router = express.Router();
 
+
 // Getting all
-router.get('/', (req, res) => {
-res.send('Hello World');
+router.get('/', async (req, res) => {
+    try {
+        const subscribers = await Subscriber.find();
+        res.json(subscribers);
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
 })
 // Getting one
 router.get('/:id', (req, res) => {
-res.send(req.params.id)
+    res.send(req.params.id)
 })
 //Creating one
 router.post('/', (req, res) => {
