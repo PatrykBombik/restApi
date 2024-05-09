4f2980312917ca5643481c6a1c6c6c3c86c04de5
Patryk Bombik <patryk.bombik@gmail.com>
2024-05-09 11:58:50 +0200

    server

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import subscribersRouter from './routes/subscribers.js';
 
 dotenv.config();
 const app = express();
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
-db.on('error', (error) => console.error(errpr));
+db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
+
+app.use(express.json());
+
+app.use('/subscribers', subscribersRouter);
 
 app.listen(3000, () => console.log('Server has started'));
 
